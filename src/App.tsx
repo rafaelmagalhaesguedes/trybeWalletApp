1b73e22c5e91d8489e8b2f6ccd90160c3971aec4
Rafael <rafaelmagalhaes1@live.com>
2023-10-02 06:15:21 -0300

    feat: add react-router-dom and create route Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+    </Routes>
+  );
 }
 
 export default App;
