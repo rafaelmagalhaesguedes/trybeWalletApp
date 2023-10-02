7568052f082da4659f1623b12cb6ad832dabc66f
Rafael <rafaelmagalhaes1@live.com>
2023-10-02 07:30:32 -0300

    feat: create redux store

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,1 +1,9 @@
-// configure aqui sua store
+//
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from 'redux-thunk';
+import Default from './reducers';
+
+const store = createStore(Default, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
