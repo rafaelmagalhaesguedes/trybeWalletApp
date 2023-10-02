98485794c374a8c519ebffa9a377b215e5923af2
Rafael <rafaelmagalhaes1@live.com>
2023-10-02 07:06:10 -0300

    feat: create Form Login

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+type LoginFormType = {
+  email: string,
+  password: string,
+};
+
+function Login() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState<LoginFormType>({
+    email: '',
+    password: '',
+  });
+  const [formValid, setFormValid] = useState<boolean>(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    const isEmailValid = validateEmail(formData.email);
+    const isPasswordValid = validatePassword();
+    setFormValid(isEmailValid && isPasswordValid);
+  };
+
+  const validatePassword = () => {
+    return formData.password.length >= 6;
+  };
+
+  const validateEmail = (email: string): boolean => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (formValid) navigate('/carteira');
+  };
+
+  return (
+    <section className="login">
+      <div className="login-logo">Logo</div>
+      <form className="login-form">
+        <input
+          data-testid="email-input"
+          type="text"
+          name="email"
+          value={ formData.email }
+          onChange={ handleChange }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          value={ formData.password }
+          onChange={ handleChange }
+        />
+        <button
+          type="submit"
+          disabled={ !formValid }
+          onClick={ () => handleSubmit }
+        >
+          Entrar
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default Login;
