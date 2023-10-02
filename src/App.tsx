1da2977061ded44fa7e90a2f103b5f53a0f131a8
Rafael <rafaelmagalhaes1@live.com>
2023-10-02 07:08:28 -0300

    feat: create route /carteira

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import Login from './pages/Login/Login';
+import Wallet from './pages/Wallet/Wallet';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
+      <Route path="/carteira" element={ <Wallet /> } />
     </Routes>
   );
 }
