391fcebd3d4d29998c5ac485cb9af1cf0859d77f
Rafael <rafaelmagalhaes1@live.com>
2023-10-02 07:35:11 -0300

    feat: create user reducer

--- src/redux/reducers/user.ts
+++ src/redux/reducers/user.ts
@@ -1,1 +1,19 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { AnyAction } from 'redux';
+
+const initialState = {
+  email: '',
+  password: '',
+};
+
+function searchReducer(state = initialState, action: AnyAction) {
+  switch (action.type) {
+    case 'LOGIN_BEGIN':
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+}
+
+export default searchReducer;
