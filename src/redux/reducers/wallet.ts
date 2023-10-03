5836bba585b2a899dfd9c29b676876275213a93d
Rafael <rafaelmagalhaes1@live.com>
2023-10-03 19:19:05 -0300

    feat: create wallet Reducer

--- src/redux/reducers/wallet.ts
+++ src/redux/reducers/wallet.ts
@@ -1,1 +1,21 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { AnyAction } from 'redux';
+import { ADD_EXPENSE } from '../actions';
+
+const INITIAL_STATE = {
+  expenses: [],
+  currencies: [],
+};
+
+const wallet = (state = INITIAL_STATE, action: AnyAction) => {
+  switch (action.type) {
+    case ADD_EXPENSE: {
+      return {
+        ...state,
+        expenses: [...state.expenses, action.payload],
+      };
+    }
+    default: return state;
+  }
+};
+
+export default wallet;
