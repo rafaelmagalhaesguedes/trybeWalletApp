3f9882dda173d880399988f63a6cee16e5e8592a
Rafael <rafaelmagalhaes1@live.com>
2023-10-03 19:07:09 -0300

    feat: create action add expense

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,8 +1,16 @@
 export const ADD_EMAIL = 'ADD_EMAIL';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
 
 export const actionAddEmail = (email: string) => (
   {
     type: ADD_EMAIL,
     payload: email,
+  }
+);
+
+export const actionAddExpense = (data: string[]) => (
+  {
+    type: ADD_EXPENSE,
+    payload: data,
   }
 );
