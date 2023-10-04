f5a27455e4b59d0ab4d4d31731bde908dce92060
Rafael <rafaelmagalhaes1@live.com>
2023-10-04 08:43:04 -0300

    feat: config wallet reducer

--- src/redux/reducers/wallet.ts
+++ src/redux/reducers/wallet.ts
@@ -1,17 +1,23 @@
-import { AnyAction } from 'redux';
-import { ADD_EXPENSE } from '../actions';
+import { ActionType, WalletType } from '../../types';
+import { ADD_EXPENSES, ADD_CURRENCIES } from '../actions';
 
-const INITIAL_STATE = {
+const INITIAL_STATE: WalletType = {
   expenses: [],
   currencies: [],
 };
 
-const wallet = (state = INITIAL_STATE, action: AnyAction) => {
+const wallet = (state = INITIAL_STATE, action: ActionType) => {
   switch (action.type) {
-    case ADD_EXPENSE: {
+    case ADD_EXPENSES: {
       return {
         ...state,
         expenses: [...state.expenses, action.payload],
+      };
+    }
+    case ADD_CURRENCIES: {
+      return {
+        ...state,
+        currencies: action.payload,
       };
     }
     default: return state;
