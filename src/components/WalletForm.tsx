bdb123ee8ea738665af67a3dba521d650b447b3a
Rafael <rafaelmagalhaes1@live.com>
2023-10-04 08:07:39 -0300

    feat: create Wallet Form

--- src/components/WalletForm.tsx
+++ src/components/WalletForm.tsx
@@ -1,129 +1,130 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { FormType, TypeDispatch } from '../types';
+import { actionCreatorExpenses, actionCreatorGetApi } from '../redux/actions';
 import { fetchCurrencies } from '../services/api';
-import { actionAddExpense } from '../redux/actions';
 
 function WalletForm() {
-  const dispatch = useDispatch();
-  const [nextId, setNextId] = useState(0);imp
-  const [formData, setFormData] = useState({
+  const dispatch: TypeDispatch = useDispatch();
+  const getCurrencies = useSelector((state: any) => state.wallet.currencies);
+  const [formData, setFormData] = useState<FormType>({
     id: 0,
-    description: '',
-    tag: '',
     value: '',
-    method: '',
+    description: '',
     currency: 'USD',
+    method: 'Dinheiro',
+    tag: 'Alimentação',
+    exchangeRates: '',
   });
 
-  const handleChange = (e:
-  React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>,
+  ) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+    // Requisição para API
     const rate = await fetchCurrencies();
-
-    // Crie um objeto de despesa
-    const expense = {
-      ...formData,
-      exchangeRates: rate,
-    };
-
-    // Dispatch da ação para adicionar a despesa ao estado global
-    dispatch(actionAddExpense(expense));
-
-    setNextId(nextId + 1);
-
-    // Limpe os campos do formulário
+    // Salva Dados no estado global junto com a Cotação da moeda
+    const Expense = { ...formData, exchangeRates: rate };
+    // Dispara action para salvar os dados
+    dispatch(actionCreatorExpenses(Expense));
+    // Reseta o formulário
     setFormData({
-      id: expense.id + 1,
-      description: '',
-      tag: 'Alimentação',
+      id: Expense.id + 1,
       value: '',
-      method: 'DINHEIRO',
-      currency: 'BRL', // Defina um valor padrão para a moeda, se necessário
+      description: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+      exchangeRates: '',
     });
   };
+  // Busca moedas da API
+  useEffect(() => {
+    dispatch(actionCreatorGetApi());
+  }, [dispatch]);
 
   return (
-    <form onChange={ handleSubmit }>
-      <div className="inputs">
+    <div>
+      <form onSubmit={ handleSubmit }>
         <label>
-          Descrição da despesa
-          {' '}
+          Value:
           <input
-            data-testid="description-input"
-            type="text"
-            name="description"
-            value={ formData.description }
-            onChange={ handleChange }
-          />
-        </label>
-
-        <label>
-          Categoria da despesa
-          {' '}
-          <select
-            data-testid="tag-input"
-            name="tag"
-            value={ formData.tag }
-            onChange={ handleChange }
-          >
-            <option value="alimentacao">Alimentação</option>
-            <option value="lazer">Lazer</option>
-            <option value="trabalho">Trabalho</option>
-            <option value="transporte">Transporte</option>
-            <option value="saude">Saúde</option>
-          </select>
-        </label>
-
-        <label>
-          Valor
-          {' '}
-          <input
-            data-testid="value-input"
-            type="text"
+            type="number"
             name="value"
+            data-testid="value-input"
             value={ formData.value }
             onChange={ handleChange }
           />
         </label>
 
         <label>
-          Método de pagamento
-          {' '}
-          <select
-            data-testid="method-input"
-            name="method"
-            value={ formData.method }
+          Description:
+          <input
+            type="text"
+            name="description"
+            data-testid="description-input"
+            value={ formData.description }
             onChange={ handleChange }
-          >
-            <option value="dinheiro">Dinheiro</option>
-            <option value="cartaoDeCredito">Cartão de crédito</option>
-            <option value="cartaoDeDebito">Cartão de débito</option>
-          </select>
+          />
         </label>
 
         <label>
-          Moeda
-          {' '}
+          Currency:
           <select
             data-testid="currency-input"
             name="currency"
             value={ formData.currency }
             onChange={ handleChange }
           >
-            <option value="dinheiro">USD</option>
+            {
+            getCurrencies.map((coin: any) => {
+              return (
+                <option key={ coin }>
+                  {coin}
+                </option>
+              );
+            })
+            }
           </select>
         </label>
-      </div>
-      <div>
+
+        <label>
+          Payment method:
+          <select
+            data-testid="method-input"
+            name="method"
+            value={ formData.method }
+            onChange={ handleChange }
+          >
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+        </label>
+
+        <label>
+          Category:
+          <select
+            data-testid="tag-input"
+            name="tag"
+            value={ formData.tag }
+            onChange={ handleChange }
+          >
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+        </label>
         <button type="submit">Adicionar despesa</button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
 
