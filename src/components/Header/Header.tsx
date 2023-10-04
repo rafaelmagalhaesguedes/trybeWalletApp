b233bc5b42c560857dc5cdcbe6ff225370ec48e7
Rafael <rafaelmagalhaes1@live.com>
2023-10-04 10:16:53 -0300

    feat: create total expenses / total-field

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,32 @@
 import { useSelector } from 'react-redux';
-import { RootState } from '../../types';
+import { RootStateType } from '../../types';
 
 function Header() {
-  const { email } = useSelector((state: RootState) => state.user);
+  const { email } = useSelector((state: RootStateType) => state.user);
+  const { expenses } = useSelector((state: RootStateType) => state.wallet);
+
+  const totalExpenses = expenses.reduce((total, expense) => {
+    // Converte Value para float
+    const expenseValue = parseFloat(expense.value);
+    // Seta a moeda para pegar o ask
+    const currencyCode = expense.currency;
+    // Pega o valor ask da cotação da moeda
+    const exchangeRate = expense.exchangeRates[currencyCode]?.ask || 1;
+    // Multiplica o valor pela cotação
+    const convertedValue = expenseValue * exchangeRate;
+    // Soma o valor convertido ao total de despesas
+    return total + convertedValue;
+  }, 0);
+
   return (
     <header>
-      <div className="logo-header">Logo</div>
-      <div className="total-field">
-        <span data-testid="total-field">
-          0
-        </span>
-        {' '}
-        <span data-testid="header-currency-field">
-          BRL
-        </span>
-      </div>
-      <div data-testid="email-field">{ email }</div>
+      <span data-testid="email-field">
+        {email}
+      </span>
+      <span data-testid="total-field">
+        { totalExpenses.toFixed(2) }
+      </span>
+      <span data-testid="header-currency-field">BRL</span>
     </header>
   );
 }
