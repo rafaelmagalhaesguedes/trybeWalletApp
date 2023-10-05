fab1561d9625a1e829ec671090f1ee9664b20b51
Rafael <rafaelmagalhaes1@live.com>
2023-10-05 07:13:35 -0300

    feat: create Delete Expenses on wallet reducer

--- src/redux/reducers/wallet.ts
+++ src/redux/reducers/wallet.ts
@@ -1,5 +1,5 @@
 import { ActionType, WalletType } from '../../types';
-import { ADD_EXPENSES, ADD_CURRENCIES } from '../actions';
+import { ADD_EXPENSES, DELETE_EXPENSES, ADD_CURRENCIES } from '../actions';
 
 const INITIAL_STATE: WalletType = {
   expenses: [],
@@ -12,6 +12,14 @@ const wallet = (state = INITIAL_STATE, action: ActionType) => {
       return {
         ...state,
         expenses: [...state.expenses, action.payload],
+      };
+    }
+    case DELETE_EXPENSES: {
+      const { payload: id } = action;
+      const updatedExpenses = state.expenses.filter((expense) => expense.id !== id);
+      return {
+        ...state,
+        expenses: updatedExpenses,
       };
     }
     case ADD_CURRENCIES: {
