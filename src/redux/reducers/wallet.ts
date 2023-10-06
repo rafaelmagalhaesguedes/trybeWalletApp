1f1a7c962f59b75b5def4d70ed6a31bccb7fdb4f
Rafael <rafaelmagalhaes1@live.com>
2023-10-06 09:06:03 -0300

    feat: config wallet reducer to Update Expense

--- src/redux/reducers/wallet.ts
+++ src/redux/reducers/wallet.ts
@@ -1,31 +1,63 @@
 import { ActionType, WalletType } from '../../types';
-import { ADD_EXPENSES, DELETE_EXPENSES, ADD_CURRENCIES } from '../actions';
+import {
+  ADD_CURRENCIE,
+  ADD_EXPENSE,
+  DELETE_EXPENSE,
+  EDIT_EXPENSE,
+  UPDATE_EXPENSE,
+} from '../actions';
 
 const INITIAL_STATE: WalletType = {
   expenses: [],
   currencies: [],
+  expenseId: null,
+  expenseUpdate: false,
 };
 
 const wallet = (state = INITIAL_STATE, action: ActionType) => {
   switch (action.type) {
-    case ADD_EXPENSES: {
+    case ADD_CURRENCIE: {
+      return {
+        ...state,
+        currencies: action.payload,
+      };
+    }
+    case ADD_EXPENSE: {
       return {
         ...state,
         expenses: [...state.expenses, action.payload],
       };
     }
-    case DELETE_EXPENSES: {
+    case DELETE_EXPENSE: {
       const { payload: id } = action;
-      const updatedExpenses = state.expenses.filter((expense) => expense.id !== id);
+      const updatedExpenses = state.expenses
+        .filter((expense) => expense.id !== id);
       return {
         ...state,
         expenses: updatedExpenses,
       };
     }
-    case ADD_CURRENCIES: {
+    case EDIT_EXPENSE: {
       return {
         ...state,
-        currencies: action.payload,
+        expenseUpdate: true,
+        expenseId: action.payload,
+      };
+    }
+    case UPDATE_EXPENSE: {
+      return {
+        ...state,
+        expenseId: null,
+        expenseUpdate: false,
+        expenses: state.expenses.map((expense) => {
+          if (expense.id === action.payload.id) {
+            return {
+              ...expense,
+              ...action.payload.expense,
+            };
+          }
+          return expense;
+        }),
       };
     }
     default: return state;
