21c613fa4a10b213e976ec67ab3eaa29a874d523
Rafael <rafaelmagalhaes1@live.com>
2023-10-06 17:28:34 -0300

    feat: config renderWithRouterAndRedux

--- src/tests/helpers/renderWith.tsx
+++ src/tests/helpers/renderWith.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import { applyMiddleware, legacy_createStore as createStore, Store } from 'redux';
 import { render } from '@testing-library/react';
 import thunk from 'redux-thunk';
@@ -45,6 +46,7 @@ export function renderWithRedux(component: React.ReactElement, options: Options 
 
   return {
     ...render(withRedux(component, store)),
+    user: userEvent.setup(),
     store,
   };
 }
