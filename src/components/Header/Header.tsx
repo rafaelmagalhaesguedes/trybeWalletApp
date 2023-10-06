42c54eb6fd3fa7b3b22027e95bf20e98d0f91672
Rafael <rafaelmagalhaes1@live.com>
2023-10-06 09:10:08 -0300

    refat: function totalExpenses on Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,21 +6,16 @@ function Header() {
   const { expenses } = useSelector((state: RootStateType) => state.wallet);
 
   const totalExpenses = expenses.reduce((total, expense) => {
-    // Converte Value para float
-    const expenseValue = parseFloat(expense.value);
-    // Seta a moeda para pegar o ask
-    const currencyCode = expense.currency;
-    // Pega o valor ask da cotação da moeda
-    const exchangeRate = expense.exchangeRates[currencyCode]?.ask || 1;
-    // Multiplica o valor pela cotação
-    const convertedValue = expenseValue * exchangeRate;
-    // Soma o valor convertido ao total de despesas
+    const exchangeRate = expense.exchangeRates[expense.currency]?.ask || 1;
+    const convertedValue = parseFloat(expense.value) * Number(exchangeRate);
     return total + convertedValue;
   }, 0);
 
   return (
     <header>
       <span data-testid="email-field">
+        Email
+        {' '}
         {email}
       </span>
       <span data-testid="total-field">
