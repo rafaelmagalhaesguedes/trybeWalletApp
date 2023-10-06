893c1fcf126a9498717de860abcd9b452ef04082
Rafael <rafaelmagalhaes1@live.com>
2023-10-06 09:05:13 -0300

    fix: create actionsEditExpense and UpdateExpense

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,36 +1,60 @@
 import { fetchCurrencies } from '../../services/api';
 import { DispatchType, ExpensesType } from '../../types';
-
+//
 export const ADD_EMAIL = 'ADD_EMAIL';
-export const ADD_EXPENSES = 'ADD_EXPENSES';
-export const DELETE_EXPENSES = 'DELETE_EXPENSES';
-export const ADD_CURRENCIES = 'ADD_CURRENCIES';
+//
+export const ADD_CURRENCIE = 'ADD_CURRENCIE';
+//
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const DELETE_EXPENSE = 'DELETE_EXPENSE';
+export const EDIT_EXPENSE = 'EDIT_EXPENSE';
+export const UPDATE_EXPENSE = 'UPDATE_EXPENSE';
 
+// Action Creator Add Email
 export const actionAddUser = (user: string) => ({
   type: ADD_EMAIL,
   payload: user,
 });
 
-export const actionAddExpenses = (expenses: ExpensesType) => ({
-  type: ADD_EXPENSES,
+// Action Creator Add Expense
+export const actionAddExpense = (expenses: ExpensesType) => ({
+  type: ADD_EXPENSE,
   payload: expenses,
 });
-
-export const actionDeleteExpenses = (id: number) => ({
-  type: DELETE_EXPENSES,
-  payload: id,
-});
-
+// Action Creator Add Currencies
 export const actionAddCurrencies = (currencies: string[]) => ({
-  type: ADD_CURRENCIES,
+  type: ADD_CURRENCIE,
   payload: currencies,
 });
 
+// Action Creator Edit Expense
+export const actionEditExpense = (id: number) => ({
+  type: EDIT_EXPENSE,
+  payload: id,
+});
+
+// Action Creator Update Expense
+export const actionUpdateExpense = (expense: ExpensesType, expenseId: number) => ({
+  type: UPDATE_EXPENSE,
+  payload: {
+    expense,
+    id: expenseId,
+  },
+});
+
+// Action Creator Delete Expense
+export const actionDeleteExpense = (id: number) => ({
+  type: DELETE_EXPENSE,
+  payload: id,
+});
+
+// Action Creator Get Currencies on API
 export function actionFetchApi() {
   return async (dispatch: DispatchType) => {
     try {
       const data = await fetchCurrencies();
-      const currencies = Object.keys(data).filter((currencie) => currencie !== 'USDT');
+      const currencies = Object.keys(data)
+        .filter((currencie) => currencie !== 'USDT');
       dispatch(actionAddCurrencies(currencies));
     } catch (error) {
       console.log(error);
