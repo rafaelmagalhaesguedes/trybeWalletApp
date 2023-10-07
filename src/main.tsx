26bf579303915fc8fe596cae0c66e6105471abf0
Rafael <rafaelmagalhaes1@live.com>
2023-10-07 14:00:17 -0300

    feat: add GlobalStyles on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux';
+import { GlobalStyle } from './GlobalStyle';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
     <BrowserRouter>
       <Provider store={ store }>
+        <GlobalStyle />
         <App />
       </Provider>
     </BrowserRouter>,
