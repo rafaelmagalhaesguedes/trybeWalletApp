47e889625bba8be976679198d39ca9d6cdf426cc
Rafael <rafaelmagalhaes1@live.com>
2023-10-07 19:53:53 -0300

    styles: initial styles WalletForm

---
+++ src/components/WalletForm/Styles.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 10px 50px;
+  height: 130px;
+  background: #E1E5EB7D;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-flow: row wrap;
+`;
+
+export const Label = styled.label`
+  padding: 20px;
+`;
+
+export const InputDesc = styled.input`
+  width: 288px;
+  height: 30px;
+  border-radius: 5px;
+`;
+
+export const Select = styled.select`
+  width: 150px;
+  height: 30px;
+  padding: 0 10px;
+  border-radius: 5px;
+`;
