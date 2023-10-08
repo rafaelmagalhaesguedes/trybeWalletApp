544b39ec97aa1bfea3e8f0509bec9628867d1a91
Rafael <rafaelmagalhaes1@live.com>
2023-10-08 07:54:29 -0300

    styles: inputs, selects and button styled

--- src/components/WalletForm/Styles.tsx
+++ src/components/WalletForm/Styles.tsx
@@ -2,25 +2,34 @@ import styled from 'styled-components';
 
 export const FormContainer = styled.div`
   display: flex;
-  justify-content: center;
-  padding: 10px 50px;
-  height: 130px;
+  padding: 20px;
+  height: 145px;
   background: #E1E5EB7D;
 `;
 
 export const Form = styled.form`
   display: flex;
   flex-flow: row wrap;
+  justify-content: center;
+  gap: 5px;
 `;
 
 export const Label = styled.label`
-  padding: 20px;
+  padding: 10px;
+  color: #003BE5;
 `;
 
 export const InputDesc = styled.input`
-  width: 288px;
+  width: 350px;
   height: 30px;
   border-radius: 5px;
+  padding: 0 10px;
+  border: 1px solid #003BE5;
+  
+  &:focus {
+    border: 2px solid blue;
+    outline: 0;
+  }
 `;
 
 export const Select = styled.select`
@@ -28,4 +37,45 @@ export const Select = styled.select`
   height: 30px;
   padding: 0 10px;
   border-radius: 5px;
+  border: 1px solid #003BE5;
+  cursor: pointer;
+  color: #003BE5;
+  
+  &:focus {
+    border: 2px solid blue;
+    outline: 0;
+  }
+`;
+
+export const InputValue = styled.input`
+  width: 240px;
+  height: 30px;
+  border-radius: 5px;
+  padding: 0 10px;
+  border: 1px solid #003BE5;
+  
+  &:focus {
+    border: 2px solid blue;
+    outline: 0;
+  }
+`;
+
+export const ButtonForm = styled.button`
+background: #2FC18C;
+  border-radius: 5px;
+  color: white;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 14px;
+  text-align: center;
+  margin-top: 37px;
+  padding: 10px;
+  height: 40px;
+  width: 330px;
+  cursor: pointer;
+  border: none;
+
+  &:hover {
+    background: linear-gradient(0deg, rgba(0,3,255,1) 0%, rgba(2,126,251,1) 90%);
+  }
 `;
